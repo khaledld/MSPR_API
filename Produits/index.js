e88643f047be40df861e506fe91ab98dafe48ce0
medaymenmehdioui <aimene.mehdioui@gmail.com>
2024-06-25 16:55:11 +0200

    first commit

---
+++ Produits/index.js
@@ -0,0 +1,179 @@
+require("dotenv").config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express")
+const cors = require("cors")
+
+const { Kafka } = require('kafkajs');
+
+
+const app = express()
+app.use(cors())
+
+app.use(express.json())
+
+
+//Controlleurs
+
+//Récupère une liste des produits. 
+const getProducts = async (req, res) =>{
+    
+
+    try{
+        const products = await client.db("Produits").collection("Produits").find()
+
+        const productsList = await products.toArray()
+    
+        res.json({productsList})
+
+    }catch(erreur){
+        console.log(erreur)
+        res.json({erreur})
+    } 
+
+}
+
+
+//Récupère un produit par son ID
+const getProductById = async (req, res) =>{
+
+    try{
+    const id = req.params.id
+
+    const product = await client.db("Produits").collection("Produits").findOne({_id:new ObjectId(id)})
+   
+    res.json({product})
+    
+    }catch(erreur){
+        console.log(erreur)
+        res.json({erreur})
+    } 
+}
+
+
+//Créé un nouveau produit
+const addNewProduct  = async (req, res) =>{
+    
+    const name = req.body.name
+    const prix = req.body.prix
+    const categorie = req.body.categorie
+
+    try{
+        await client.db("Produits").collection("Produits").insertOne({name, prix, categorie})
+        res.json({msg:"New product Added"})
+
+    }catch(erreur){
+        console.log(erreur)
+        res.json({erreur})
+    } 
+}
+
+
+//Met à jour une fiche produit
+const updateProductById = async (req, res) =>{
+
+    //change prix only
+    const id = req.params.id
+    const prix = req.body.prix
+
+    try{
+
+    await client.db("Produits").collection("Produits").updateOne({_id:new ObjectId(id)}, {$set:{prix}})
+   
+    res.json({msg:"Product updated"})
+
+    }catch(erreur){
+        console.log(erreur)
+        res.json({erreur})
+    } 
+}
+
+
+//Supprimer Produit by Id
+const deleteProductById = async (req, res) =>{
+    const id = req.params.id
+
+    try{
+    await client.db("Produits").collection("Produits").deleteOne({_id:new ObjectId(id)})
+
+    const producer = kafka.producer()
+
+    await producer.connect()
+    await producer.send({
+      topic: 'deletedProducts',  // deja creer en kafka en CMD
+      messages: [
+        { value: id},
+      ],
+    })
+    
+    await producer.disconnect()
+    
+    res.json({msg:"Product deleted"})
+
+    }catch(erreur){
+        console.log(erreur)
+        res.json({erreur})
+} 
+}
+
+
+
+//Routs
+app.get("/products", getProducts)
+
+app.get("/products/:id", getProductById)
+
+
+app.post("/products", addNewProduct)
+
+app.put("/products/:id", updateProductById)
+
+app.delete("/products/:id", deleteProductById)
+
+
+//-------------------------------------------------------------------------------------------------------
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    app.listen(process.env.SERVERPORT, console.log("Product API Running on Port : "+ process.env.SERVERPORT ))
+
+  } catch(erreur){
+
+    console.log(erreur)
+
+  }
+}
+
+run().catch(console.dir);
+
+
+
+
+const kafka = new Kafka({
+    clientId: 'MSPR2',
+    brokers: ['localhost:9092']
+  });
+
+
+
+
+
+
+
+
+
+
